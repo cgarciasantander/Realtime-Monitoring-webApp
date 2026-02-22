22f25dec152b56757d534e61883a1d108e8c6f03
Manuel Santiago Prieto Hernandez <m.prietoh@uniandes.edu.co>
2026-02-21 19:15:06 -0500

    add endpoint api/city-activity/

--- realtimeMonitoring/realtimeGraph/urls.py
+++ realtimeMonitoring/realtimeGraph/urls.py
@@ -13,4 +13,5 @@ urlpatterns = [
     path("login/", LoginView.as_view(), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
     path("historical/data", download_csv_data, name="historical-data"),
+    path("api/city-activity/", city_activity, name="city-activity"),
 ]
